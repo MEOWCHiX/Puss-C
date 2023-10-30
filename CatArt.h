ff1db7334a8c9926b634a01d31bf260aca69da96
MEOWCHiX <meowchix@gmail.com>
2023-10-30 15:02:19 -0400

    Create CatArt.h

---
+++ CatArt.h
@@ -0,0 +1,11 @@
+#ifndef CAT_ART_H
+#define CAT_ART_H
+
+// A majestic ASCII representation of a cat to grace the code with a feline presence.
+#define CAT_ART R"(\
+    /\_/\  
+   ( o.o ) 
+    > ^ <
+)"
+
+#endif // CAT_ART_H
